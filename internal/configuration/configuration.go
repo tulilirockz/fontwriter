d1925224c56e6852bf502b9786089be7c98048b7
tulili <tulilirockz.pub+git@gmail.com>
2023-07-20 11:19:42 -0300

    feat: decimal scaling factor

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -1,18 +1,17 @@
 package configuration
 
 type Text struct {
-	Dpi            int    `json:"dpi"`
-	Size           int    `json:"size"`
-	Hinting        string `json:"hinting"`
-	Scaling_factor int    `json:"scaling_factor"`
-	Font_path      string `json:"font_path"`
+	Dpi            int     `json:"dpi"`
+	Size           int     `json:"size"`
+	Hinting        string  `json:"hinting"`
+	Scaling_factor float32 `json:"scaling_factor"`
+	Font_path      string  `json:"font_path"`
 }
 
 type Output struct {
-	Letters_per_frame int    `json:"letters_per_frame"`
-	Anti_aliasing     bool   `json:"anti_aliasing"`
-	Gif               bool   `json:"gif"`
-	Path              string `json:"path"`
+	Letters_per_frame int  `json:"letters_per_frame"`
+	Anti_aliasing     bool `json:"anti_aliasing"`
+	Gif               bool `json:"gif"`
 }
 
 type ProgramConf struct {
